044f5bce1b01eb32fb149a19c4246651287569fd
Alexander Gerasimov <alexg.school@mail.ru>
2020-04-17 12:25:13 +0300

    last version module_0

---
+++ module_0/README.md
@@ -0,0 +1,5 @@
+Task: write a program that guesses the number from a given interval in a minimum number of attempts
+
+This program finds the hidden number for the minimum number of attempts and shows the average number of attempts per 1000 experiments. This program using the binary search algorithm.
+ 
+You can see the program code in the file guess_the_number.py and the result of program execution in the file guess_the_number.ipynb
---
+++ module_0/guess_the_number.py
@@ -0,0 +1,43 @@
+
+def score_game(game_core_v3):
+    
+    """
+    Function starts the game 1000
+    The guessed number is from 1 to 100
+    Returns the average number of required attempts
+    
+    """
+    import numpy as np
+    count_ls = []
+    np.random.seed(1)  
+    random_array = np.random.randint(1, 101, size=(1000))
+    
+    for number in random_array:
+        count_ls.append(game_core_v3(number))    
+    score = int(np.mean(count_ls))
+    
+    return(print(f"Алгоритм угадывает число в среднем за {score} попыток"))
+
+
+def game_core_v3(number):
+    
+    """
+    The implementation of the binary search algorithm for the hidden number
+    The function takes the requested number and returns the number of attempts
+
+    """
+    first = 1 
+    last = 100
+    count = 0
+    import math
+    
+    while first <= last:
+        count += 1
+        predict = math.trunc((first + last)/2)
+        if predict == number:
+            return count
+        elif predict > number:
+            last = predict - 1
+        else:
+            first = predict + 1
+
